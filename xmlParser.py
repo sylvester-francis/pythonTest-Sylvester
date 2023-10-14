0c9e517c3b4df41ea5307abd9bc6844dd96a3727
Sylvester Francis <sylvesterranjithfrancis@gmail.com>
2023-10-13 20:04:48 -0400

    Basic stub functions

--- xmlParser.py
+++ xmlParser.py
@@ -4,5 +4,29 @@ and saves the results back to the xml file and generates reports from the xml fi
 
 Author: Sylvester Ranjith Francis
 Date created : 10/13/2023
+Last modified by: Sylvester Ranjith Francis
+last modified date: 10/13/2023
 '''
 
+def parse_XML():
+    pass
+def increase_price():
+    pass
+
+def rename_category():
+    pass
+
+def remove_products():
+    pass
+
+def save_changes():
+    pass
+
+def generate_reports():
+    pass
+
+def menu():
+    pass
+
+if __name__ == '__main__':
+    menu()
