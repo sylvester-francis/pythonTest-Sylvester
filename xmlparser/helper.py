e410afee0e245bf693bd47f1027cff93b8e336df
Sylvester Francis <sylvesterranjithfrancis@gmail.com>
2023-10-15 02:13:03 -0400

    Fixed min,max rating not found

--- xmlparser/helper.py
+++ xmlparser/helper.py
@@ -4,3 +4,7 @@ def return_args():
     parser.add_argument('filepath', type=str, help="Please provide the path to the file containing the xml product data")
     args = parser.parse_args()
     return args
+def return_min_max_rating():
+    min_rating=0
+    max_rating=5
+    return min_rating,max_rating
--- xmlparser/product_manipulation.py
+++ xmlparser/product_manipulation.py
@@ -1,4 +1,6 @@
 from prettytable import PrettyTable
+from helper import return_min_max_rating
+min_rating,max_rating =return_min_max_rating()
 
 def increase_price(products):
     try:
