ae03529365ae08652e800dd603185b3b017576d8
Sylvester Francis <sylvesterranjithfrancis@gmail.com>
2023-10-15 03:18:13 -0400

    Added comments to utility_functions.py and xml_operations.py

--- xmlparser/utility_functions.py
+++ xmlparser/utility_functions.py
@@ -1,10 +1,31 @@
+'''
+This module contains the utility functions
+(1). return_args
+(2). return_min_max_ratings
+
+Author: Sylvester Ranjith Francis
+Date created : 10/15/2023
+Last modified by: Sylvester Ranjith Francis
+last modified date: 10/15/2023
+'''
 import argparse
+
+# Function to parse command-line arguments and return them
 def return_args():
+    # Create an ArgumentParser object
     parser = argparse.ArgumentParser(description="XML Product Data parser")
+    # Add a positional argument for the file path
     parser.add_argument('filepath', type=str, help="Please provide the path to the file containing the xml product data")
+    # Parse the command-line arguments
     args = parser.parse_args()
+    # Return the parsed arguments
     return args
+
+# Function to return the minimum and maximum rating values
 def return_min_max_rating():
-    min_rating=0
-    max_rating=5
-    return min_rating,max_rating
+    # Set the minimum rating value
+    min_rating = 0
+    # Set the maximum rating value
+    max_rating = 5
+    # Return the minimum and maximum rating values as a tuple
+    return min_rating, max_rating
